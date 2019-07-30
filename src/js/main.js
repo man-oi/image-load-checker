c9ad72f91d910f73ccada57349750025dec331a5
manoi <git@manuelbartels.de>
2019-07-30 21:01:47 +0200

    es6+ style

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,17 @@
 'use strict';
 
-// var imgLoader = new imagesloaded({
-//     imageSelector   : 'img',
-//     imageLoaded     : function(element) {
-//         element.parentNode.classList.add('loaded');
-//     },
-//     imageError      : function(element) {
-//         element.parentNode.classList.add('error');
-//     }
-// });
-//
-// imgLoader.init();
-
-
 ImageLoadChecker({
-    imageLoaded: function(element) {
+    imageLoaded(element) {
         element.parentNode.classList.add('loaded');
     },
-    imageLoadedAfterTimeout: function(element) {
+    imageLoadedAfterTimeout(element) {
         element.parentNode.classList.add('loaded');
         element.style.opacity = 0.5;
     },
-    imageError: function(element) {
+    imageError(element) {
         element.parentNode.classList.add('error');
     },
-    imageErrorAfterTimeout: function(element) {
+    imageErrorAfterTimeout(element) {
         element.parentNode.classList.add('error');
     },
     timeoutTime: 2000
